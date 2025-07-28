075f47231b5e467a3c6ef503bee86a0850422cef
Chelike Nikhil <100200794+nikhil32047@users.noreply.github.com>
2025-07-28 10:55:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <hr></hr>
+      <h1>Welcome to React/ReactJs</h1>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default App;
